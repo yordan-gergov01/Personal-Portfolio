558b5381728b4d67200eb837e049e7d5ba7c3e3e
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-10 18:31:42 +0200

    render the navigation menu

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
 
+import { navLinks } from "../constants/index";
+
+function NavItems() {
+  return (
+    <ul className="nav-ul">
+      {navLinks.map((item) => (
+        <li key={item.id} className="nav-li">
+          <a href={item.href} className="nav-li-a" onClick={() => {}}>
+            {item.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen((open) => !open);
+  }
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
@@ -14,11 +34,25 @@ function Navbar() {
             Yordan
           </a>
 
-          <button>
-            <img src={"assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="toggle"
+              className="w-6 h-6"
+            />
           </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
         </div>
       </div>
+
+      <div></div>
     </header>
   );
 }
