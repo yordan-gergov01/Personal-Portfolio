c8f9062171f75423832251b8b635af901c44f15d
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-11 15:20:25 +0200

    displaying the object on the page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,19 @@
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "three";
-import HackerRoom from "../components/DeveloperRoom";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
 import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
 
 function Hero() {
+  const controls = useControls("HackerRoom", {
+    rotationX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+  });
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -16,11 +26,19 @@ function Hero() {
       </div>
 
       <div className="w-full h-full absolute inset-0">
+        <Leva />
         <Canvas className="w-full h-full">
-          <Suspense fallback={null}></Suspense>
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
 
-          <HackerRoom />
+            <HackerRoom
+              scale={[0.1, 0.1, 0.1]}
+              position={[2, -8, 2]}
+              rotation={[0, -Math.PI, 0]}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
         </Canvas>
       </div>
     </section>
