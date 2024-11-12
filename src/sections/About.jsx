2d0feed15cbe937c6ca3f789e3ca5f7500344bb0
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-12 18:27:48 +0200

    finishing the about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,20 @@
 import Globe from "react-globe.gl";
 import Button from "../components/Button";
+import { useState } from "react";
 
 function About() {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  function handleCopy() {
+    navigator.clipboard.writeText("yordansoftskills@gmail.com");
+
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  }
+
   return (
     <section className="c-space my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
@@ -78,6 +91,27 @@ function About() {
                 I love solving problems and building things through code. Coding
                 isn&apos;t just my hobby or profession - it is my passion.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  yordansoftskills@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
