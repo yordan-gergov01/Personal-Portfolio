7d1eba770a470f3964eea4d82a44281d7b10629b
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-12 17:53:37 +0200

    display the earth with text for my working time

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,5 @@
+import Globe from "react-globe.gl";
+
 function About() {
   return (
     <section className="c-space my-20">
@@ -30,6 +32,34 @@ function About() {
             />
             <div>
               <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in JavaScript with focus on React and Node.js
+                ecosystems.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                width={326}
+                height={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">I can work remotely or on-site</p>
+              <p className="grid-subtext">
+                I&apos;m based in Veliko Tarnovo, Bulgaria, with remote work
+                available or on-site in other place.
+              </p>
             </div>
           </div>
         </div>
