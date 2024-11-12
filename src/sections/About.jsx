5f9d5569458942fbdaab11db1f0435ff6b44f2e1
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-12 16:03:02 +0200

    display the avatar with personal info

---
+++ src/sections/About.jsx
@@ -0,0 +1,28 @@
+function About() {
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/mygrid.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">Hi, I&apos;m Yordan</p>
+              <p className="grid-subtext">
+                With 1 year of experience, I have honed my skills in front-end
+                and back-end development, with a focus on real world apps and
+                better user experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
