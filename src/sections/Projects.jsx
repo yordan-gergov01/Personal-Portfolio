e6ae476b97844129e179af872270a4c1fb66964e
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-14 17:54:26 +0200

    displaying and import computer model

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { myProjects } from "../constants";
+import { Canvas } from "@react-three/fiber";
+import { Center } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import DemoComputer from "../components/DemoComputer";
 
 const projectCount = myProjects.length;
 
@@ -92,7 +96,19 @@ function Projects() {
           </div>
         </div>
 
-        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full"></div>
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <Canvas>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 5]} />
+            <Center>
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0, 1, 0]}>
+                  <DemoComputer />
+                </group>
+              </Suspense>
+            </Center>
+          </Canvas>
+        </div>
       </div>
     </section>
   );
