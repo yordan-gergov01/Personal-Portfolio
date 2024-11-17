ef82a05226b3ae79bec4d189af8ae5fd8d7b72e1
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-17 16:58:38 +0200

    Putting animations on avatar

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,9 +1,46 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 
-function Developer(props) {
+function Developer({ animationName = "idle", ...props }) {
+  const group = useRef();
   const { nodes, materials } = useGLTF("/models/animations/developer.glb");
+
+  const { animations: idleAnimation } = useFBX("/models/animations/idle.fbx");
+  const { animations: saluteAnimation } = useFBX(
+    "/models/animations/salute.fbx"
+  );
+  const { animations: clappingAnimation } = useFBX(
+    "/models/animations/clapping.fbx"
+  );
+  const { animations: victoryAnimation } = useFBX(
+    "/models/animations/victory.fbx"
+  );
+
+  idleAnimation[0].name = "idle";
+  saluteAnimation[0].name = "salute";
+  clappingAnimation[0].name = "clapping";
+  victoryAnimation[0].name = "victory";
+
+  const { actions } = useAnimations(
+    [
+      idleAnimation[0],
+      saluteAnimation[0],
+      clappingAnimation[0],
+      victoryAnimation[0],
+    ],
+    group
+  );
+
+  useEffect(
+    function () {
+      actions[animationName].reset().fadeIn(0.5).play();
+
+      return () => actions[animationName].fadeOut(0.5);
+    },
+    [animationName]
+  );
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={group}>
       <primitive object={nodes.Hips} />
       <skinnedMesh
         name="EyeLeft"
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { workExperiences } from "../constants";
 import { OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import CanvasLoader from "../components/CanvasLoader";
 import Developer from "../components/Developer";
 
 function Experience() {
+  const [animationName, setAnimationName] = useState("idle");
+
   return (
     <section className="c-space my-20">
       <div className="w-full text-white-600">
@@ -19,7 +21,11 @@ function Experience() {
               <directionalLight position={[10, 10, 10]} intensity={1} />
               <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
               <Suspense fallback={<CanvasLoader />}>
-                <Developer />
+                <Developer
+                  position-y={-3}
+                  scale={3}
+                  animationName={animationName}
+                />
               </Suspense>
             </Canvas>
           </div>
@@ -27,7 +33,15 @@ function Experience() {
             <div className="sm:py-10 py-5 sm:px-5 px-2.5">
               {workExperiences.map(
                 ({ id, name, pos, icon, duration, title, animation }) => (
-                  <div key={id} className="work-content_container group">
+                  <div
+                    key={id}
+                    className="work-content_container group"
+                    onClick={() => setAnimationName(animation.toLowerCase())}
+                    onPointerOver={() =>
+                      setAnimationName(animation.toLowerCase())
+                    }
+                    onPointerOut={() => setAnimationName("idle")}
+                  >
                     <div className="flex flex-col h-full justify-start items-center py-2">
                       <div className="work-content_logo">
                         <img src={icon} alt="logo" className="w-full h-full" />
